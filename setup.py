9971d2e48d5ea0b6de1d545954b848e7dc76caf4
wangfei <gf0842wf@gmail.com>
2015-04-09 15:13:41 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+classifiers = [
+    "Programming Language :: Python 2.7",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+setup(
+    name='tu',
+    packages=[
+        'tu',
+    ],
+    description='Tornado utils',
+    license='MIT',
+    author='fk',
+    author_email='gf0842wf@gmail.com',
+    keywords=['tornado', 'util'],
+    classifiers=classifiers,
+)
